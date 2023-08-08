6ae8608abc407e7c1c6f4ce0c6b249b8be03a40b
sani4618 <sanijoseph2018@gmail.com>
2023-08-08 11:22:10 +0530

    header is completed

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './component/Home';
 import Search from './component/Search';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-
-      <Home/>
-      <Search/>
-     
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/"exact element={<Home/>}/>
+      <Route path="/search"exact element={<Search/>}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Header from './Header'
 
 const Home = () => {
   return (
     <div>
+        <Header/>
 
         <div className="container">
             <div className="row">
