579122558cf3b12da0764f3632487b0045383dc9
sani4618 <sanijoseph2018@gmail.com>
2023-08-08 11:51:38 +0530

    image added

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -16,7 +16,7 @@ const Home = () => {
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
                         <div class="card">
-  <img src="..." class="card-img-top" alt="..."/>
+  <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg" class="card-img-top" alt="..."/>
   <div class="card-body">
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
   </div>
