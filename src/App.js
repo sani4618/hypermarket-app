f9a5d176e54906001a2797e066754d7e7f4bd305
sani4618 <sanijoseph2018@gmail.com>
2023-08-08 10:53:31 +0530

    Search is completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './component/Home';
+import Search from './component/Search';
 
 function App() {
   return (
     <div>
 
       <Home/>
+      <Search/>
      
     </div>
   );
